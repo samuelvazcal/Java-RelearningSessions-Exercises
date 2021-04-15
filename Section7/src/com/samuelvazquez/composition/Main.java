958a309e8166d76de1c8129e1f773d24cccfb564
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-04-14 23:23:27 -0500

    Add composition example

---
+++ Section7/src/com/samuelvazquez/composition/Main.java
@@ -0,0 +1,15 @@
+package com.samuelvazquez.composition;
+
+public class Main {
+    public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(20,20,5);
+        Case theCase = new Case("220B","Dell","240", dimensions);
+        Monitor monitor = new Monitor("27inch Beast","Acer",27,new Resolution(2540,1440));
+        Motherboard motherboard = new Motherboard("BJ-200","Asus",4,6,"v2.44");
+
+        PC thePC = new PC(theCase,monitor,motherboard);
+        thePC.getMonitor().drawPixelAt(1500,1200,"blue");
+        thePC.getMotherboard().loadProgram("Ubuntu 19.0");
+        thePC.getTheCase().pressPowerButton();
+    }
+}
---
+++ Section7/src/com/samuelvazquez/inheritance/Vehicle.java
@@ -0,0 +1,9 @@
+package com.samuelvazquez.inheritance;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}

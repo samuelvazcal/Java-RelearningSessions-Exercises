4f35d684b164f666a0d11c6565a8d2721050482d
Samuel Vazquez <samuel_vazquez@epam.com>
2021-04-16 17:30:20 -0500

    Add extra comment for static nested classes

--- Section9/src/com/samuelvazquez/nestedclasses/staticnestedclasses/Main.java
+++ Section9/src/com/samuelvazquez/nestedclasses/staticnestedclasses/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		//static inner class, which means that you can access it without creating an object of the outer class
 		OuterClass.InnerClass myInner = new OuterClass.InnerClass();
+		//static inner class does not have access to members of the outer class
 		System.out.println(myInner.y);
 	}
 }

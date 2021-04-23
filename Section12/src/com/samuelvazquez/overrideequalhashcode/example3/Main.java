a50b6cb70063392e24834469aac89b11eef108a6
Samuel Vazquez <samuel_vazquez@epam.com>
2021-04-23 15:31:17 -0500

    Add final example of the importance of overriding equals and hashCode methods

--- Section12/src/com/samuelvazquez/overrideequalhashcode/example3/Main.java
+++ Section12/src/com/samuelvazquez/overrideequalhashcode/example3/Main.java
@@ -1,5 +1,8 @@
 package com.samuelvazquez.overrideequalhashcode.example3;
 
+import java.util.HashSet;
+
+
 public class Main {
 	public static void main(String[] args) {
 		//although the two instances have exactly the same attribute values, they are stores in different locations.
@@ -8,8 +11,32 @@ public class Main {
 		Student st1 = new Student(1,"Samuel");
 		Student st2 = new Student(1,"Samuel");
 
+		//And now, after overriding equals method, both student are considered equals because of its id
+		//even when they don't have the same hashCode
 		System.out.println("student1 hashCode = " + st1.hashCode());
 		System.out.println("student2 hashCode = " + st2.hashCode());
 		System.out.println("Checking equality between st1 and st2 = " + st1.equals(st2));
+
+		//So, what's the purpose of overriding hashCode()?.
+		//What if we want to all students in a HashSet?
+		HashSet<Student> students = new HashSet<Student>();
+		students.add(st1);
+		students.add(st2);
+
+		//contains method will return false, because HashSet still consider the objects as different.
+		//HashSet stores its elements in memory buckets. Each bucket is linked to a particular hash code.
+		//So, st1 is stored by Java inside a bucket and links it to the value of st.hashCode().
+		//So, any time an element with the same hash code is inserted into the set, it will replace st1.
+		//However, st2 has a different hash code, and it will be stored in a separate bucket,
+		//so, it will be considered a totally different object.
+		System.out.println("HashSet size = " + students.size());
+		System.out.println("HashSet contains Alex = " + students.contains(new Student(1,"Samuel")));
+
+		//That's why is important to override hashCode method, so let's override it in Student
+		//and set it to be equal to the ID
+		//so, students who have the same ID are stored in the same bucket
+		//Now, the two elements are now considered as equal ans stored in the same memory bucket.
+		//The same applied for HashMap, HashTable or any data structure that uses a hashing mechanism for storing elements
+
 	}
 }
--- Section12/src/com/samuelvazquez/overrideequalhashcode/example3/Student.java
+++ Section12/src/com/samuelvazquez/overrideequalhashcode/example3/Student.java
@@ -1,5 +1,8 @@
 package com.samuelvazquez.overrideequalhashcode.example3;
 
+import java.util.Objects;
+
+
 public class Student {
 	private int id;
 	private String name;
@@ -15,5 +18,22 @@ public class Student {
 
 	public String getName() {
 		return name;
+	}
+
+	//for business purposes, we consider that two students are equal if they have the same ID,
+	//so we override the equals() method and provide our own implementation as the following:
+	@Override
+	public boolean equals(Object o) {
+		if(this == o) return true;
+		if(o == null || this.getClass() != o.getClass()) return false;
+		Student s = (Student) o;
+		return this.id == s.id;
+		//above implementation two students are equal if and only if they are stored in the same address
+		//OR they have the same ID.
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
 }

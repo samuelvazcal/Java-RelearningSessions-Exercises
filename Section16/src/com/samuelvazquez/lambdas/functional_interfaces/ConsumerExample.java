7efaa13b88695a8506bff4e475d2df7768cf7c4b
Samuel Vazquez <samuel_vazquez@epam.com>
2021-04-28 15:07:02 -0500

    add Consumer interface implementations

--- Section16/src/com/samuelvazquez/lambdas/functional_interfaces/ConsumerExample.java
+++ Section16/src/com/samuelvazquez/lambdas/functional_interfaces/ConsumerExample.java
@@ -1,6 +1,7 @@
 package com.samuelvazquez.lambdas.functional_interfaces;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -13,15 +14,27 @@ public class ConsumerExample {
 		display.accept(10);
 
 		System.out.println("~~~~~~~~~~~~~~~\n");
+
 		//Consumer to multiply 2 to every integer of a list
 		Consumer<List<Integer>> modify = list -> {
+			int total = 0;
 			for(int i = 0; i<list.size();i++) {
 				list.set(i,2*list.get(i));
+				total += list.get(i);
 			}
+			System.out.println(total);
 		};
 
+		modify.accept(Arrays.asList(1,2,3,4,5,6,7));
+
+		System.out.println("~~~~~~~~~~~~~~~\n");
+
 		//Consumer to display a list of numbers
-		Consumer<List<Integer>> dispList = list -> list.stream().forEach(a -> System.out.println(a + " "));
+		Consumer<List<Integer>> dispList = list -> list.forEach(a -> System.out.print(a + " "));
+
+		dispList.accept(Arrays.asList(2,4,6,8,10,12,14,16,18,20));
+
+		System.out.println("\n~~~~~~~~~~~~~~~\n");
 
 		List<Integer> list = new ArrayList<Integer>();
 		list.add(2);
@@ -33,5 +46,9 @@ public class ConsumerExample {
 
 		//Implement dispList using accept()
 		dispList.accept(list);
+
+		System.out.println("~~~~~~~~~~~~+");
+		modify.andThen(dispList).accept(list);
+
 	}
 }
---
+++ Section16/src/com/samuelvazquez/lambdas/functional_interfaces/ConsumerExample2.java
@@ -0,0 +1,33 @@
+package com.samuelvazquez.lambdas.functional_interfaces;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+
+public class ConsumerExample2 {
+	public static void main(String[] args) {
+
+		// Consumer to multiply 2 to every integer of a list
+		Consumer<List<Integer>> modify = list -> {
+			for(int i = 0; i < list.size();i++) {
+				list.set(i,2*list.get(i));
+			}
+		};
+
+		// Consumer to display a list of integers
+		Consumer<List<Integer>> dispList = list -> {
+			list.forEach(a -> System.out.print(a + " "));
+		};
+
+		List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7));
+
+		// andThen() return a composed Consumer wherein the parameterized
+		// Consumer will be executed after the first one
+		modify.andThen(dispList).accept(list);
+		System.out.println("\n~~~~~~~~~");
+		dispList.andThen(modify).accept(list);
+
+	}
+}

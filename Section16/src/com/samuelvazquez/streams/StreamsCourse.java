adfb35eb09662f56e77c14652e385d3c9a8a0336
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-05-09 17:51:15 -0500

    add new implementation for streams

---
+++ Section16/src/com/samuelvazquez/streams/StreamsCourse.java
@@ -0,0 +1,27 @@
+package src.com.samuelvazquez.streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsCourse {
+    public static void main(String[] args) {
+        List<String> someBingoNumbers = Arrays.asList("N40","N36","B12","B6","G53","G49","G60","G50","g64","I26","I17","I29","O71");
+        someBingoNumbers.stream().forEach(number -> {
+            if(number.toUpperCase().startsWith("G")) {
+                System.out.print(number + " ");
+            }
+        });
+//        List<String> result = someBingoNumbers.stream().filter(s -> s.startsWith("G")).collect(Collectors.toList());
+//        System.out.println(result);
+
+        System.out.println("\nSorting the list:");
+//        List<String> sortedList =
+//                someBingoNumbers.stream().filter(s -> s.toUpperCase().startsWith("G")).sorted().collect(Collectors.toList());
+//        System.out.println(sortedList);
+        List<String> sortedList =
+                someBingoNumbers.stream().map(String::toUpperCase).filter(s -> s.startsWith("G")).collect(Collectors.toList());
+        System.out.println(sortedList);
+
+    }
+}

c7043790c56c913926e3f709f83b48158542418b
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-05-14 19:03:33 -0500

    add new case scenarios for unit testing cases

--- Section18/src/com/samuelvazquez/unittesting/BankAccountTest.java
+++ Section18/src/com/samuelvazquez/unittesting/BankAccountTest.java
@@ -1,5 +1,7 @@
 package com.samuelvazquez.unittesting;
 
+import org.junit.rules.ExpectedException;
+
 import static junit.framework.TestCase.*;
 
 /**
@@ -7,38 +9,62 @@ import static junit.framework.TestCase.*;
  */
 public class BankAccountTest {
 
+    private BankAccount account;
+    private static int count;
+
+    @org.junit.BeforeClass
+    public static void beforeClass() {
+        System.out.println("This executes before any test cases. Count = " + count++);
+    }
+
+    @org.junit.Before
+    public void setup() {
+        account = new BankAccount("Samuel","Vazquez",1000.00,BankAccount.CHECKING);
+        System.out.println("Running a test!");
+    }
+
     @org.junit.Test
     public void deposit() {
-        BankAccount account = new BankAccount("Samuel","Vazquez",1000.00,BankAccount.CHECKING);
         double balance = account.deposit(200.0,true);
         assertEquals(1200.00,balance,0);
     }
 
     @org.junit.Test
-    public void withdraw() {
-        BankAccount account = new BankAccount("Samuel","Vazquez",1000.00,BankAccount.CHECKING);
-        double balance = account.withdraw(100.00,false);
-        assertEquals(900.00,balance,0);
+    public void withdraw_branch()throws Exception {
+        double balance = account.withdraw(600.00,true);
+        assertEquals(400.00,balance,0);
+    }
+
+    @org.junit.Test(expected = IllegalArgumentException.class)
+    public void withdraw_notBranch() throws Exception{
+        account.withdraw(600.00,false);
     }
 
     @org.junit.Test
     public void getBalance_deposit() {
-        BankAccount account = new BankAccount("Samuel","Vazquez",1000.00,BankAccount.CHECKING);
         account.deposit(200.0,true);
         assertEquals(1200.00,account.getBalance(),0);
     }
 
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount account = new BankAccount("Samuel","Vazquez",1000.00,BankAccount.CHECKING);
         account.withdraw(200.0,true);
         assertEquals(800.00,account.getBalance(),0);
     }
 
     @org.junit.Test
     public void isChecking_true() {
-        BankAccount account = new BankAccount("Samuel","Vazquez",1000.00,BankAccount.CHECKING);
         assertTrue("The account is NOT a checking account", account.isChecking());
+    }
+
+    @org.junit.AfterClass
+    public static void afterClass() {
+        System.out.println("This executes after any test cases. Count = " + count++);
+    }
+
+    @org.junit.After
+    public void teardown() {
+        System.out.println("Count = " + count++);
     }
 
 }

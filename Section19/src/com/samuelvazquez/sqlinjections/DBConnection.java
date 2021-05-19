8b923492aa1ed45095492667bc83f5681a5367a8
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-05-18 21:36:52 -0500

    add enhanced conection with the database using PreparedStatement preventing SQL injection

--- Section19/src/com/samuelvazquez/sqlinjections/DBConnection.java
+++ Section19/src/com/samuelvazquez/sqlinjections/DBConnection.java
@@ -33,19 +33,30 @@ public class DBConnection {
     public void printUserData(String id, String pwd) throws NullPointerException, SQLException {
         Statement stmt;
         ResultSet rs;
+        PreparedStatement ps;
+        String query = "SELECT name, country, password FROM users WHERE email = ? AND password = ?";
         DBConnection dbConnection = new DBConnection();
 
         try {
             dbConnection.open();
-            stmt = connection.createStatement();
-            String query = "select name, country, password from users where email='"+id+"' and password='"+pwd+"'";
-            System.out.println(query);
-            rs = stmt.executeQuery(query);
+            //stmt = connection.createStatement();
+            //now using PreparedStatement
+            ps = connection.prepareStatement(query);
+            //SQL injection: samuel@apple.com' or '1'='1
+            //String query = "select name, country, password from users where email='"+id+"' and password='"+pwd+"'";
+            //System.out.println(query);
+            //rs = stmt.executeQuery(query);
+
+            //setting parameters
+            ps.setString(1,id);
+            ps.setString(2,pwd);
+            rs = ps.executeQuery();
             while(rs.next()) {
                 System.out.println("Name="+rs.getString("name")+",country="+rs.getString("country")+",password="+rs.getString("password"));
             }
             rs.close();
-            stmt.close();
+            //stmt.close();
+            ps.close();
             connection.close();
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());

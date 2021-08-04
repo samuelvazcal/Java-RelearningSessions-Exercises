1979a0e7e30ca3b8e5d88634627381cb6de779dd
Samuel Vazquez <samuelvazcal@hotmail.com>
2021-08-03 19:10:59 -0500

    added new examples for stream operations

--- Section16/src/com/samuelvazquez/lambdasqs/StreamsPersons.java
+++ Section16/src/com/samuelvazquez/lambdasqs/StreamsPersons.java
@@ -2,6 +2,7 @@ package src.com.samuelvazquez.lambdasqs;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.OptionalDouble;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -44,6 +45,21 @@ public class StreamsPersons {
         List<Person> personListB = list.stream().filter(x -> x.getAge()> 20 && x.getAge()<30).collect(Collectors.toList());
         personListB.forEach(System.out::println);
 
+        System.out.println("\nYears since they turned 18");
+        //List<Person> yearsSince18 = list.stream().map(s -> s.getAge()-18).forEach(System.out::println);
+        //list.stream().forEach(s -> System.out.println(s));
+        list.forEach(s -> {
+            int targetYears = s.getAge() - 18;
+            System.out.println(s.getFirstName() + "\t" + targetYears);
+        });
 
+        System.out.println("\n Sum of all years");
+        int totalYears = list.stream().map(Person::getAge).reduce(0, Integer::sum);
+        System.out.println("\nTotal is: " + totalYears);
+
+        System.out.println("\n AVG age");
+        OptionalDouble avgYears2 = list.stream().mapToInt(Person::getAge).average();
+        if(avgYears2.isPresent()) System.out.println(avgYears2.getAsDouble());
+        //System.out.println(avgYears2.getAsDouble());
     }
 }
